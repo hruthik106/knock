1e8133a313b3e7baa2d2610d50bba9e4d7a5ab18
Hruthik N <hruthikn106@gmail.com>
2026-01-01 22:43:20 +0530

    feat (knock) : performed http check with timeout and exit code

--- cmd/knock/main.go
+++ cmd/knock/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -14,8 +16,28 @@ func main() {
 	url := os.Args[1]
 	fmt.Println("knocking on : ", url)
 
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println(os.Stderr, "unreachable url : ", url)
+		os.Exit(3)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		fmt.Println("successfully knocked on : ", url)
+		fmt.Println("responded with : ", resp.Status)
+		os.Exit(0)
+	}
+
+	fmt.Println("X ", resp.Status)
+	os.Exit(1)
+
 }
 
 func printUsage() {
-	fmt.Println(os.Stderr, "usage : knock <url>")
+	fmt.Println(os.Stderr, "Usage: knock <url>")
 }
